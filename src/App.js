dd688fd44b562e586fc22489e089ef90e025b69d
Milton Clark <kingcoder247@gmail.com>
2021-11-19 12:29:34 -0500

    Add - Wrote outline for methods to handle future functionality for app, create products list with all details, used formatPrice helper to format all pricing on products list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import "./App.css";
 import productData from "./data/productData";
-
+import formatPrice from "./helpers/formatPrice";
 
 
 class App extends Component {
@@ -9,13 +9,46 @@ class App extends Component {
     super()
     this.state = {
       productsList: productData,
-      
+
     }
   }
 
+  handleAddToCart= () => {
+
+  }
+
+  handleFormSubmit= (e) => {
+    e.preventDefault();
+  }
+
+  
+
   render(){
+    
+    let productsListArr = this.state.productsList.map((product)=>{
+      let {name, price, img, description} = product;
+      return(
+        <div>
+          <h3>{name}</h3>
+          <div>Price: {formatPrice(price)}</div>
+          <button>Add To Cart</button>
+          <img src={img} alt={name}/>
+          <div>{description}</div>
+        </div>
+      )
+    })
+
+
     return(
-      <h1>My Garage Sale</h1>
+      <div>
+        <h1>My Garage Sale</h1>
+        <div>
+          {productsListArr}
+        </div>
+      </div>
+      
+
+
   
     )
   };
